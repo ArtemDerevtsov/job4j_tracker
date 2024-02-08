005abb9fe8b2cc1753b138b002ff7013920e6d8a
ArtemDerevtsov <a.derevcov@mail.ru>
2024-02-09 01:21:07 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error one = new Error();
+        one.printInfo();
+        Error two = new Error(true, 1, "Error detected");
+        two.printInfo();
+        Error three = new Error(false, 2, "No errors found");
+        three.printInfo();
+    }
+
+}
