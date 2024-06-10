0b7bcf0258745fb3763616f45b0aa8dec9c6e55d
ArtemDerevtsov <a.derevcov@mail.ru>
2024-06-11 01:56:18 +0700

    1. Singleton [#94606 #499146]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -20,7 +20,23 @@ public class SingleTracker {
         return tracker.add(item);
     }
 
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
     public Item findById(int id) {
-        return null;
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
     }
 }
